68b41df7c314fd4107efd4a202cae1f283b263a5
Skues <alfieskues2006@gmail.com>
2025-08-04 15:17:30 +0100

    Changes to app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import json
+import datetime as datetime
 ## TODO - 
 ACTIVITYLEVELS = {"none": 1.2, "little": 1.375, "moderate":1.55, "active":1.725, "high":1.9}
 # Fitness app
 class FitnessApp:
     def __init__(self, gender: str, weight: float, height: float, age: int) -> None:
         self.info = {"gender": gender, "weight": weight, "height": height, "age":age}
-        self.target: float = 0
+        self.daily: float = 0
 
     def _bmrCalc(self, info: dict) -> float:
         # Basal Metabolic Rate - Harris-Benedict Equation
@@ -24,22 +26,21 @@ class FitnessApp:
         return (multi * bmr)
 
     def setGoal(self, calories : float,  goal: str):
-
         match goal:
             case "bulk":
-                self.target = calories + 500
+                self.daily = calories + 500
             case "cut":
-                self.target = calories - 500
+                self.daily = calories - 500
             case _:
-                self.target = calories
+                self.daily = calories
 
     def setMacros(self) -> dict:
-        print(self.target)
+        print(self.daily)
         poundBW = self.weightConvert(self.info["weight"], "lbs")
         proteins = round(poundBW)
         fats = 0.4*poundBW
         print(fats)
-        carbs = round((self.target - (proteins*4) - (fats*9))/4)
+        carbs = round((self.daily - (proteins*4) - (fats*9))/4)
 
 
         return {"proteins": [proteins, proteins*4], "fats": [fats, fats*9], "carbs": [carbs, carbs*4]}
@@ -51,9 +52,17 @@ class FitnessApp:
                 return weight*2.205
         return 0.0
 
+    def trackFood(self, name: str, weight: float, calories: int):
+        print(f"Eaten {name} and was {calories} calories.")
+        self.daily =- calories
+        receipt = json.dumps({"datetime": "Add the current date here", "foodName":name, "weight": weight, "calorieAmount":calories, "caloriesLeft":self.daily})  
+        with open("foodeaten.json", "a") as f:
+            f.write(receipt)
+
 app = FitnessApp("Male", 75, 178, 21)
 calories = app.tdee("moderate")
 print("TDEE", calories)
 app.setGoal(calories, "bulk")
 macros = app.setMacros()
 print(macros)
+app.trackFood("Chicken and chips", 1, 1000)
